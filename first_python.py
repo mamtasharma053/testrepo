56f161de653066eaf41332bce322e62fd2d5a7df
mamtasharma053 <sharmamamta053@gmail.com>
2022-11-11 20:42:03 +0530

    Add files via upload
    Uploading python file

---
+++ first_python.py
@@ -0,0 +1,12 @@
+import matplotlib
+class Rectangle(object):
+        def __init__(self,width=2,height =3,color='r'):
+                                  self.height=height
+                                  self.width=width
+                                  self.color=color
+                                  print(width)
+        def drawRectangle(self):
+                       import matplotlib.pyplot as plt
+                       plt.gca().add_patch(plt.Rectangle((0, 0),self.width, self.height ,fc=self.color))
+                       plt.axis('scaled')
+                       plt.show()
